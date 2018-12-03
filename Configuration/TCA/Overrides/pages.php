4c4ab400742fc931bd84aa770217c91f1456eb6e
Stephan Reuther <s.reuther@veljus.de>
2018-12-03 13:18:16 +0100

    [Task] Enable visibility of `fe group deny` in page properties

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,11 +1,11 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-$additionalColumns = array(
-    'fe_group_deny' => array(
+$additionalColumns = [
+    'fe_group_deny' => [
         'exclude' => 1,
         'label' => 'LLL:EXT:denyfegroup/Resources/Private/Language/db.xml:fe_group_deny',
-        'config' => array(
+        'config' => [
             'enableMultiSelectFilterTextfield' => true,
             'renderType' => 'selectMultipleSideBySide',
             'type' => 'select',
@@ -14,11 +14,10 @@ $additionalColumns = array(
             'size' => 5,
             'minitems' => 0,
             'maxitems' => 50,
-            'items' => array(
-            ),
-        )
-    ),
-);
+            'items' => []
+        ]
+    ],
+];
 
-// \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages', $additionalColumns);
-// \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('pages', 'fe_group_deny', '', 'after:fe_group');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages', $additionalColumns);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('pages', 'fe_group_deny', '', 'after:fe_group');
